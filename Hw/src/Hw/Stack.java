55d1a94b34e038dc97b1f9c68f0c449aef36c946
Emin Tekin <emint2719gmail.com>
2024-03-24 22:28:51 +0300

    Update Stack.java
    content of 'Stack' class is filled in this commit

--- Hw/src/Hw/Stack.java
+++ Hw/src/Hw/Stack.java
@@ -2,4 +2,58 @@ package Hw;
 
 public class Stack {
 
+	private Object[] elements;
+	
+	private int top;
+	
+	public Stack(int capacity) 
+	{
+		elements=new Object[capacity];
+		top=0;
+		
+	}
+	public boolean Push(Object input) 
+	{
+		if(!isfull()) 
+		{
+			elements[top]=input;
+			top++;
+			return true;
+		}
+		return false;
+		
+	}
+	public Object Peek()
+	{
+		top--;
+		Object trns=elements[top];
+		top++;
+		return trns;
+	}
+	public Object Pop() 
+	{
+		top--;
+		Object trns=elements[top];
+		return trns;
+		
+			
+		
+	}
+	
+	public boolean isempty() 
+	{
+		return  top==0;
+	}
+	public boolean isfull() 
+	{
+		return  top==elements.length;
+	}
+	public int size() 
+	{
+		return top;
+	}
+	public int capacity() 
+	{
+		return elements.length;
+	}
 }
