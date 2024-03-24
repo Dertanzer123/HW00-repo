ed9ee74c8b2c3e93138417db2eb2a508d1fa276f
Emin Tekin <emint2719gmail.com>
2024-03-24 23:13:36 +0300

    Update Main.java
    main class is filled with Stacks , Queues, and needed paths

--- Hw/src/Hw/Main.java
+++ Hw/src/Hw/Main.java
@@ -2,4 +2,29 @@ package Hw;
 
 public class Main {
 
+	
+	String dirpath1;//dirpath for highscoretable
+	String dirpath2;//dirpath for animalnames
+
+	CQueue QName;
+	CQueue QSroce;
+	
+	Stack AnimalStack;
+	Stack LetterStack;
+	Stack WordStack;
+	Stack BoardStack;
+	Stack MissingLetterStack;
+	
+	Stack transferStack;// this is an tool for every other stack and its capacity should at least equal the biggest other Stack
+	
+	
+	
+	public Main() 
+	{
+	
+		
+		
+		
+	}
+	
 }
