da341d459ba9321cbbd688ff381f90d14abc1bfd
Emin Tekin <emint2719gmail.com>
2024-03-26 15:21:03 +0300

    update Main
    displayStack method and input letter control were added

--- Hw/src/Hw/Main.java
+++ Hw/src/Hw/Main.java
@@ -4,81 +4,129 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
-
+import java.util.Scanner;
 
 public class Main {
 
 	String dirpath1;//dirpath for highscoretable
 	String dirpath2;//dirpath for animalnames
-	Random rnd =new Random();
+	Random rnd = new Random();
 
-	CQueue QName=new CQueue(12);
-	CQueue QScore=new CQueue(12);
-	
-	Stack AnimalStack=new Stack(14);
-	Stack LetterStack=new Stack(26);
+	CQueue QName = new CQueue(12);
+	CQueue QScore = new CQueue(12);
+
+	Stack AnimalStack = new Stack(14);
+	Stack LetterStack = new Stack(26);
 	Stack WordStack;
 	Stack BoardStack;
-	Stack MissingLetterStack=new Stack(26);
-	
-	Stack transferStack=new Stack(40);// this is an tool for every other stack and its capacity should at least equal the biggest other Stack
-	
-	
-	
-	
-	public Main() throws IOException 
-	{   
-		
-		///////////////////////////----(1) in this part data from files are coppied to queues and stacks
-		FileReader Fr1= new FileReader(dirpath1);
-		BufferedReader Br1=new BufferedReader(Fr1);
+	Stack MissingLetterStack = new Stack(26);
+
+	Stack transferStack = new Stack(40);// this is an tool for every other stack and its capacity should at least equal
+										// the biggest other Stack
+
+	boolean flag1;
+	boolean flag2;
+
+	public Main() throws IOException {
+
+		/////////////////////////// ----(1) in this part data from files are coppied to
+		/////////////////////////// queues and stacks
+		FileReader Fr1 = new FileReader(dirpath1);
+		BufferedReader Br1 = new BufferedReader(Fr1);
 		String line;
-		while((line = Br1.readLine()) != null)
-		{
+		while ((line = Br1.readLine()) != null) {
 			QScore.Qadd(Integer.parseInt(line.trim().split(" ")[1]));
 			QName.Qadd(line.trim().split(" ")[0]);
-		
+
 		}
 		Br1.close();
 		Fr1.close();
-		
-		FileReader Fr2= new FileReader(dirpath2);
-		BufferedReader Br2=new BufferedReader(Fr1);
-		while((line = Br2.readLine()) != null)
-		{
-			
+
+		FileReader Fr2 = new FileReader(dirpath2);
+		BufferedReader Br2 = new BufferedReader(Fr2);
+		while ((line = Br2.readLine()) != null) {
+
 			AnimalStack.Push(line.trim());
-		
+
 		}
 		Br2.close();
 		Fr2.close();
-		//////////////////////////////------(1)
-		/////////////////////////////--------(2) this section will prepare the stacks for the game and its should be remaked before every game
-		for(int i=rnd.nextInt(14);i>0;i--) 
-		{
+		////////////////////////////// ------(1)
+		///////////////////////////// --------(2) this section will prepare the stacks
+		////////////////////////////// for the game and its should be remaked before
+		////////////////////////////// every game
+		for (int i = rnd.nextInt(14); i > 0; i--) {
 			transferStack.Push(AnimalStack.Pop());
 		}
-		line=(String)AnimalStack.Peek();
-		while(!transferStack.isempty())
-		{
-		AnimalStack.Push(transferStack.Pop());	
+		line = (String) AnimalStack.Peek();
+		while (!transferStack.isempty()) {
+			AnimalStack.Push(transferStack.Pop());
 		}
-		WordStack=new Stack(line.trim().length());
-		BoardStack=new Stack(line.trim().length());
-		for(char a:line.trim().toCharArray()) 
-		{
-		WordStack.Push(a);
-			
+		WordStack = new Stack(line.trim().length());
+		BoardStack = new Stack(line.trim().length());
+		for (char a : line.trim().toCharArray()) {
+			WordStack.Push(a);
+			BoardStack.Push('-');
 		}
-		for(int i=65;i<91;i++) 
-		{
-			LetterStack.Push((char)i);
+		for (int i = 65; i < 91; i++) {
+			LetterStack.Push((char) i);
 		}
+
+		///////////////////////////// --------(2)
+		///////////////////////////// ---------(3)
+		Scanner scan1 = new Scanner(System.in);
+		char a;
 		
-		/////////////////////////////--------(2)
-		
-		
-		
+		while (true) {
+			System.out.println("guees an letter , find which animal it is");
+			displayStack(BoardStack);
+			System.out.print("     usable letters:");
+			displayStack(LetterStack);
+			System.out.print("     used letters:");
+			displayStack(MissingLetterStack);
+			System.out.println();
+			a = scan1.nextLine().toUpperCase().charAt(0);
+			flag1 = true;
+			flag2 = true;
+			if (a < 65 || a > 90) {
+				flag2 = false;
+			}
+
+			while (!MissingLetterStack.isempty()) {
+
+				if (MissingLetterStack.Peek().equals(a)) {
+					flag1 = false;
+					break;
+				}
+				transferStack.Push(MissingLetterStack.Pop());
+
+			}
+			while (!transferStack.isempty()) {
+				MissingLetterStack.Push(transferStack.Pop());
+			}
+			if (!flag2) {
+				System.out.println("this input is not valid!!");
+			} else if (!flag1) {
+				System.out.println("you already tried this letter!!");
+			} else {
+
+			}
+		}
+		///////////////////////////// ---------(3)
+
 	}
-	
+
+	public void displayStack(Stack input) {
+		while (!input.isempty()) {
+			transferStack.Push(input.Pop());
+
+		}
+		while (!transferStack.isempty()) {
+			System.out.print(transferStack.Peek());
+			input.Push(transferStack.Pop());
+
+		}
+
+	}
+
 }
--- Hw/src/Hw/test.java
+++ Hw/src/Hw/test.java
@@ -1,5 +1,11 @@
 package Hw;
 
+import java.io.IOException;
+
 public class test {
 
+	public static void main(String[] args) throws IOException {
+		Main mymain=new Main();
+	}
+	
 }
