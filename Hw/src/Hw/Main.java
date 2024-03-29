8ef363ca637f4b8f452d5ee6ff9dbf539268a9a1
Emin Tekin <emint2719gmail.com>
2024-03-29 17:40:47 +0300

    Update Main.java
    ı add more fuctions for scoreboard and score arangement

--- Hw/src/Hw/Main.java
+++ Hw/src/Hw/Main.java
@@ -12,8 +12,8 @@ public class Main {
 	String dirpath2;//dirpath for animalnames
 	Random rnd = new Random();
 
-	CQueue QName = new CQueue(12);
-	CQueue QScore = new CQueue(12);
+	CQueue QName;
+	CQueue QScore;
 
 	Stack AnimalStack = new Stack(14);
 	Stack LetterStack = new Stack(26);
@@ -21,8 +21,8 @@ public class Main {
 	Stack BoardStack;
 	Stack MissingLetterStack = new Stack(26);
 
-	Stack transferStack = new Stack(40);// this is an tool for every other stack and its capacity should at least equal
-										// the biggest other Stack
+	Stack transferStack = new Stack(50);// this is an tool for every other stack and its capacity should at least equal
+	Stack transferStack2= new Stack(50);								// the biggest other Stack
 	boolean[] control1;
 
 	boolean flag1;
@@ -40,14 +40,57 @@ public class Main {
 		FileReader Fr1 = new FileReader(dirpath1);
 		BufferedReader Br1 = new BufferedReader(Fr1);
 		String line;
+		int Qlenght = 0;
+		int max = 0;
 		while ((line = Br1.readLine()) != null) {
-			QScore.Qadd(Integer.parseInt(line.trim().split(" ")[1]));
+			Qlenght++;
+			if (max < Integer.parseInt(line.trim().split(" ")[1])) {
+				max = Integer.parseInt(line.trim().split(" ")[1]);
+			}
+		}
+		Br1.close();
+		Fr1.close();
+		Fr1 = new FileReader(dirpath1);
+		Br1 = new BufferedReader(Fr1);
+		QName = new CQueue(Qlenght+10);
+		QScore = new CQueue(Qlenght+10);
+		Br1 = new BufferedReader(Fr1);
+		while ((line = Br1.readLine()) != null) {
 			QName.Qadd(line.trim().split(" ")[0]);
+			QScore.Qadd(Integer.parseInt(line.trim().split(" ")[1]));
 
 		}
 		Br1.close();
 		Fr1.close();
-
+		while (!QScore.isempty()) {
+			for (int i = QScore.size(); i > 0; i--) {
+				if (QScore.Peek().equals(max)) {
+					transferStack.Push(QScore.Qtake());
+					transferStack.Push(QName.Qtake());
+				} else {
+					QScore.Qadd(QScore.Qtake());
+					QName.Qadd(QName.Qtake());
+				}
+			}
+			max = 0;
+			for (int i = QScore.size(); i > 0; i--) {
+				if (max < (int) QScore.Peek()) {
+					max = (int) QScore.Peek();
+				}
+				QScore.Qadd(QScore.Qtake());
+			}
+		}
+		while(!transferStack.isempty()) 
+		{
+			transferStack2.Push(transferStack.Pop());
+			
+		}
+		while(!transferStack2.isempty()) 
+		{
+			QScore.Qadd(transferStack2.Pop());
+			QName.Qadd(transferStack2.Pop());
+		}
+		transferStack2=null;
 		FileReader Fr2 = new FileReader(dirpath2);
 		BufferedReader Br2 = new BufferedReader(Fr2);
 		while ((line = Br2.readLine()) != null) {
@@ -61,8 +104,9 @@ public class Main {
 		///////////////////////////// --------(2) this section will prepare the stacks
 		////////////////////////////// for the game and its should be remaked before
 		////////////////////////////// every game
+		displayscores();
 		Scanner scan1 = new Scanner(System.in);
-		replay=true;
+		replay = true;
 		while (replay) {
 			start: ;
 			for (int i = rnd.nextInt(14); i > 0; i--) {
@@ -100,16 +144,13 @@ public class Main {
 					displayStack(MissingLetterStack);
 					System.out.print("     Hp : " + hp);
 					System.out.println();
-					if(end(true,scan1)) 
-					{
-						replay=true;
-					}
-					else 
-					{
-						replay=false;
+					if (end(true, scan1)) {
+						replay = true;
+					} else {
+						replay = false;
 					}
 					break;
-					
+
 				} else if (hp <= 0) {
 					System.out.println("Hah loser!!");
 					displayStack(BoardStack);
@@ -119,17 +160,13 @@ public class Main {
 					displayStack(MissingLetterStack);
 					System.out.print("     Hp : " + hp);
 					System.out.println();
-					if(end(false,scan1)) 
-					{
-						replay=true;
-					}
-					else 
-					{
-						replay=false;
+					if (end(false, scan1)) {
+						replay = true;
+					} else {
+						replay = false;
 					}
 					break;
-				} 
-				else {
+				} else {
 					while (true) {// flag1 is used to know is input letter used allready,flag2 is used for knowing
 									// is input a letter or naa
 
@@ -141,16 +178,15 @@ public class Main {
 						displayStack(MissingLetterStack);
 						System.out.print("     Hp : " + hp);
 						System.out.println();
-						flag3=true;
-						while(flag3) {
-						try {
-							flag3=false;
-						a = scan1.nextLine().charAt(0);
-						}
-						catch (Exception e) {
-							System.out.println("please enter something dont just push the enter button.");
-							flag3=true;
-						}
+						flag3 = true;
+						while (flag3) {
+							try {
+								flag3 = false;
+								a = scan1.nextLine().charAt(0);
+							} catch (Exception e) {
+								System.out.println("please enter something dont just push the enter button.");
+								flag3 = true;
+							}
 						}
 						if (a < 65 || a > 90) {
 							a -= 32;
@@ -197,7 +233,7 @@ public class Main {
 						}
 					}
 					flag1 = true;////// flag1 in here used for is a is a missedletter or naa
-					
+
 					// < check a==letterstack letters remove equal one
 					while (!LetterStack.isempty()) {
 						if (LetterStack.Peek().equals(a)) {
@@ -291,7 +327,14 @@ public class Main {
 	public boolean end(boolean fate, Scanner scan)// if winned fate== true else false
 	{
 		if (fate) {
-			System.out.println("brawoooo my friend you soo good play brawoo, i'll put your name on board.");
+			System.out.println("brawoooo my friend you soo good play brawoo, i'll put your name on board.whats your name?");
+			String name;
+			do
+			{
+				name=scan.nextLine();	
+			}while(name.trim()==null);
+			arangeelement(hp, name);
+			displayscores();
 			System.out.println("you already win but you can play again.");
 		} else {
 			System.out.println("i didnot see anybody worse like you hahaha!");
@@ -308,9 +351,45 @@ public class Main {
 				return false;
 			} else {
 				System.out.println("please say yes or no,i dont understant any other.");
-				
+
 			}
 		}
 	}
 
+	public void arangeelement(int score, String name) {
+		boolean flag4=true;
+		for (int i = QScore.size(); i > 0; i--) {
+			
+			if(flag4&&score>(int)QScore.Peek())
+			{
+				QScore.Qadd(score);
+				QName.Qadd(name);
+				
+				flag4=false;
+			}
+			QScore.Qadd(QScore.Qtake());
+			QName.Qadd(QName.Qtake());
+			
+		}
+
+	}
+
+	public void displayQueue(CQueue input) {
+		for (int i = input.size(); i > 0; i--) {
+			System.out.print(" "+input.Peek()+" ");
+			
+			input.Qadd(input.Qtake());
+		}
+		
+	}
+	public void displayscores() {
+		for (int i = QScore.size(); i > 0; i--) {
+			System.out.println("||"+QScore.Peek()+" : "+QName.Peek());
+			
+			
+			QScore.Qadd(QScore.Qtake());
+			QName.Qadd(QName.Qtake());
+		}
+		
+	}
 }
